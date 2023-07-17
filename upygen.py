1fc1e93b87096bd4dba83647fba48be006984995
Haley Lepe <hlepe754@icloud.com>
2023-07-17 15:24:09 -0700

    add initial python code generation and execution

--- upygen.py
+++ upygen.py
@@ -7,8 +7,6 @@ import tiktoken
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-query = sys.argv[1]
-
 def num_tokens_from_string(string: str, encoding_name="cl100k_base") -> int:
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
@@ -30,18 +28,20 @@ def classify(query):
     prompt += f"Query: \"{query}\"\n"
     prompt += f"Class: "
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
         temperature=0,
         max_tokens=1,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0,
-        stop=["\"\"\""]
+
     )
 
-    classification = response["choices"][0]["text"]
+    classification = response["choices"][0]["message"]["content"]
 
     if not ("valid" in classification and not "invalid" in classification):
         return False
@@ -69,27 +69,73 @@ def rewrite_prompt(q):
     rewrite += f"Original: \"{q}\"\n"
     rewrite += f"Rewritten: "
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=rewrite,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": rewrite}
+        ],
         max_tokens=num_tokens_from_string(rewrite) + num_tokens_from_string(q) + num_tokens_from_string("Python code that uses the uclid5_api package") ,
         frequency_penalty=-2.0,
         presence_penalty=-2.0,
-        stop=["\"\"\""]
+
     )
 
-    rewritten_prompt = response["choices"][0]["text"]
+    rewritten_prompt = response["choices"][0]["message"]["content"]
 
     return rewritten_prompt
 
+def run_query(query):
+    api_prompt = query
+    api_prompt += "\nThe first line must be \"from uclid5_api import *\"."
+    api_prompt += "\nThe second line must be \"m = Module(\"main\")\"."
+    api_prompt += "\nThe last line must be \"print(m)\"."
+    api_prompt += "\n\"Module\" is a class that models a transition system in UCLID5. You will use a single Module called \"m\"."
+    api_prompt += "\nThe \"Module\" method \"declare_var\" takes a name (string) and a UCLID5 type, and returns a new variable."
+    api_prompt += "\nFor example, \"x = m.declare_var(\"x\", integer())\" creates an integer variable called \"x\"."
+    api_prompt += "\nThe \"Module\" field \"init\" represents the initialization block of the transition system."
+    api_prompt += "\nThe \"Module\" field \"next\" represents the transition relation block of the transition system."
+    api_prompt += "\nBoth \"init\" and \"next\" are object of type \"Block\"."
+    api_prompt += "\n\"Block\" objects have two methods: \"assign\" and \"branch\"."
+    api_prompt += "\n\"assign\" takes two arguments: a variable, representing the left-hand-side of an assignment; and an expression, representing the right-hand-side of an assignment."
+    api_prompt += "\nFor example, you can assign \"x\" the value \"0\" in the init block with \"m.init.assign(x, 0)\"."
+    api_prompt += "\nFor example, you can assign \"x\" the value \"x + x\" in the next block with \"m.next.assign(x, x + x)\"."
+    api_prompt += "\n\"branch\" takes one argument, a boolean expression, and returns two objects of type \"Block\"."
+    api_prompt += "\nFor example, you can branch on the value of \"x\" being greater than zero with \"then_, else_ = m.next.branch(x > 0)\"."
+    api_prompt += "\nNow use this information about the uclid5_api to write the correct Python code."
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": api_prompt}
+        ],
+        max_tokens=num_tokens_from_string(api_prompt) + num_tokens_from_string(query),
+    )
+
+    python_code = response["choices"][0]["message"]["content"]
+
+    return python_code
+
+
+
 
 if __name__ == "__main__":
-    if classify(query):
-        query2 = rewrite_prompt(query).strip()
-        # TODO: actually run the query now. Need to:
-        # - Add info about uclid5_api to prompt
-        # - Maybe add COT structure or something
-        print(query2)
-    else:
+    original_query = sys.argv[1]
+
+    if not classify(original_query):
         print("Sorry, I can only handle queries that ask for UCLID5 code.")
+
+    rewritten_query = rewrite_prompt(original_query).strip()
+
+    code = run_query(rewritten_query)
+
+    print("\\\\ Rewritten query: ")
+    print("\\\\ " + "\n\\\\ ".join(rewritten_query.splitlines()))
+    print("\\\\ Python code to run: ")
+    print("\\\\" + "\n\\\\ ".join(code.splitlines()))
+    print("\\\\ Resulting UCLID5 code: ")
+
+    try:
+        exec(code)
+    except:
+        print("Ooops, there was an error in the generated Python code :(")
 
